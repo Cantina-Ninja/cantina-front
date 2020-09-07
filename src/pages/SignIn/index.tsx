46ce1a99b6fc538badd3d783ed275df3bbfa644c
Flavio Ever <flavio.ever@outlook.com>
2020-09-07 02:54:41 -0300

    Teste de validação

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { RiUser6Line, RiKey2Fill } from 'react-icons/ri';
-import * as yup from 'yup';
+import * as Yup from 'yup';
 import { Container, Content, Background, Header, Form } from './styles';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-  const signIn = async (data: object) => {
-    // { email: 'test@example.com', password: '123456' }
-    const schema = yup.object().shape({
-      user: yup.string().required('Usuário não preenchido'),
-      password: yup.string().required('Senha não preenchida'),
-    });
-    const teste = await schema.isValid(data);
-    console.log(teste);
-  };
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        user: Yup.string().required('Usário obrigatório'),
+        password: Yup.string().required('Senha obrigatório'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    console.log(data);
+  }, []);
 
   return (
     <Container>
       <Background />
 
       <Content>
-        <Form onSubmit={signIn}>
+        <Form onSubmit={handleSubmit}>
           <Header>
             <div>
               <h2>Bem vindo(a) ao</h2>
