78ca5ee847a40442774fa9cd7448663728505b13
Flavio Ever <flavio.ever@outlook.com>
2020-09-07 02:02:13 -0300

    Componentização SignIn + Fix responsividade

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,51 +1,51 @@
 import React from 'react';
 import { RiUser6Line, RiKey2Fill } from 'react-icons/ri';
-import { Form } from '@unform/web';
-import Logo from '../../assets/logo.svg';
-
-import { Main } from './styles';
-
+import * as yup from 'yup';
+import { Container, Content, Background, Header, Form } from './styles';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import Logo from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-  function handleSubmit(data: any) {
+  const signIn = async (data: object) => {
     // { email: 'test@example.com', password: '123456' }
-    console.log(data);
-  }
+    const schema = yup.object().shape({
+      user: yup.string().required('Usuário não preenchido'),
+      password: yup.string().required('Senha não preenchida'),
+    });
+    const teste = await schema.isValid(data);
+    console.log(teste);
+  };
 
   return (
-    <Main>
-      <div className="container-bg" />
-      <div className="wapper-signin">
-        <div className="signin-container">
-          <header>
+    <Container>
+      <Background />
+
+      <Content>
+        <Form onSubmit={signIn}>
+          <Header>
             <div>
               <h2>Bem vindo(a) ao</h2>
               <h1>CantinaNinja</h1>
             </div>
             <img src={Logo} alt="Logotipo" />
-          </header>
-          <footer>
-            <Form onSubmit={handleSubmit}>
-              <Input
-                name="user"
-                icon={RiUser6Line}
-                type="text"
-                placeholder="Usuario"
-              />
-              <Input
-                name="password"
-                icon={RiKey2Fill}
-                type="password"
-                placeholder="Senha"
-              />
-              <Button type="submit">Entrar</Button>
-            </Form>
-          </footer>
-        </div>
-      </div>
-    </Main>
+          </Header>
+          <Input
+            name="user"
+            icon={RiUser6Line}
+            type="text"
+            placeholder="Usuario"
+          />
+          <Input
+            name="password"
+            icon={RiKey2Fill}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Entrar</Button>
+        </Form>
+      </Content>
+    </Container>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,62 +1,72 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import { Form as form } from '@unform/web';
 import bgSignIn from '../../assets/bgSignIn.jpg';
 
-export const Main = styled.div`
-  display: flex;
-
-  width: 100vw;
+export const Container = styled.div`
   height: 100vh;
 
-  .container-bg {
-    flex: 65%;
+  display: flex;
+  align-items: stretch;
+`;
 
-    background: url(${bgSignIn}) no-repeat;
-    background-size: cover;
+const fromLeft = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100px)
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const Content = styled.div`
+  animation: ${fromLeft} 1s;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 800px;
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${bgSignIn}) no-repeat center;
+  background-size: cover;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  flex-direction: row;
+  line-height: 1.3;
+  margin-bottom: 35px;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+
+  img {
+    margin-left: 25px;
+    width: 6rem;
   }
 
-  .wapper-signin {
-    flex: 35%;
-    display: flex;
-    flex-direction: column;
+  h2 {
+    font-size: 1.2rem;
+  }
+  h1 {
+    color: #fff;
+    font-size: 2rem;
+  }
+`;
 
-    align-items: center;
-    justify-content: center;
+export const Form = styled(form)`
+  flex: flex;
+  flex-direction: row;
+  width: 50%;
 
-    .signin-container {
-      flex: flex;
-      flex-direction: row;
-      width: 60%;
-
-      header {
-        display: flex;
-        flex-direction: row;
-        line-height: 1.3;
-        margin-bottom: 35px;
-
-        align-items: center;
-
-        img {
-          margin-left: 25px;
-        }
-
-        h2 {
-          font-size: 1.2rem;
-        }
-        h1 {
-          color: #fff;
-          font-size: 2rem;
-        }
-      }
-
-      footer {
-        div + div {
-          margin-top: 12px;
-        }
-      }
-
-      button {
-        margin-top: 35px;
-      }
-    }
+  button {
+    margin-top: 35px;
   }
 `;
