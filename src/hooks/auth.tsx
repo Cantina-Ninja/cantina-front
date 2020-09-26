0016d60972413b10b6cf6fe5430d0835ae7b14a4
Flavio Ever <flavio.ever@outlook.com>
2020-09-26 02:00:38 -0300

    Retirando 400 - Falha de servidor

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -65,11 +65,7 @@ const AuthProvider: React.FC = ({ children }) => {
   api.interceptors.response.use(
     response => response,
     error => {
-      if (
-        error.response.status === 403 ||
-        error.response.status === 400 ||
-        error.response.status === 401
-      ) {
+      if (error.response.status === 403 || error.response.status === 401) {
         signOut();
       }
     },
