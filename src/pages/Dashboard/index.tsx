cf781732168a6b08d61b07c8b8c23e56acbf4435
Flavio Ever <flavio.ever@outlook.com>
2020-11-07 15:11:55 -0300

    Get dashboard array

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,43 @@
-import React, { useLayoutEffect, useEffect } from 'react';
+import React, {
+  useLayoutEffect,
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
 import ChartLine from '../../components/ChartLine';
 import Card from '../../components/Card';
 import { Main, ContainerChart, CardsContainer } from './styles';
 
-const Dashboard: React.FC = () => {
-  const data = [
-    1500,
-    200,
-    7000,
-    100,
-    700,
-    900,
-    500,
-    4000,
-    500,
-    4000,
-    500,
-    4000,
-  ];
+import api from '../../services/api';
+import Load from '../../components/Load';
 
+const Dashboard: React.FC = () => {
+  const [dashboard, setDashboard] = useState<any[]>([]);
   const { innerWidth }: any = window;
+
+  const getDashboad = useCallback(async () => {
+    try {
+      const { data } = await api.get<any>('dashboard');
+
+      setDashboard(data);
+    } catch (error) {
+      console.warn(error);
+    }
+  }, []);
 
   useLayoutEffect(() => {
     console.log(innerWidth);
   }, [innerWidth]);
 
+  useEffect(() => {
+    getDashboad();
+  }, []);
+
   return (
     <Main>
       <header>Dashboard</header>
       <ContainerChart>
-        <ChartLine height={100} data={data} />
+        <ChartLine height={100} data={dashboard} />
       </ContainerChart>
       <hr />
       <CardsContainer>
